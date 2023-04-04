191333860bd75d9080b8be5a3bd55dbf53d0e4ef
mymy19 <129322146+mymy19@users.noreply.github.com>
2023-04-04 12:01:44 +0700

    Add files via upload

---
+++ BT32.py
@@ -0,0 +1,11 @@
+M1=int(input('M1='))
+M2=int(input('M2='))
+M3=int(input('M3='))
+S=int(input('S='))
+import math
+if S<=100:
+    print('Phai tra=',S*M1,sep='')
+elif 100 < S <= 150:
+    print('Phai tra=',100*M1 + (S-100)*M2,sep='')    
+else:
+    print('Phai tra=', 100*M1 + 50*M2 + (S-150)*M3,sep='')
